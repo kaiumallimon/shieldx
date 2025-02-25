2d9deda3cbb2b63044ed379dd8c61547f8665dbd
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-25 22:49:53 +0600

    project setup

---
+++ lib/app/core/constants/_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets{
+  // Logo 
+  static const String logosmall = 'assets/images/1.png';
+  static const String logobig = 'assets/images/2.png';
+  static const String logoWithName = 'assets/images/3.png';
+
+  // Onboarding
+  static const String onboarding1 = 'assets/images/4.png';
+  static const String onboarding2 = 'assets/images/5.png';
+  static const String onboarding3 = 'assets/images/6.png';
+}
