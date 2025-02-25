ad55ca35184e1a2df463a949b4b8b6dd7db54b7f
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-26 00:05:09 +0600

    Add AppColors class for centralized color management in the application

---
+++ lib/app/core/constants/_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3541f2);
+  static const Color darkPrimary = CupertinoColors.activeOrange;
+  static const Color secondary = Color(0xFFf68712);
+  static const Color darkSecondary = CupertinoColors.activeBlue;
+  static const Color surface = Colors.white;
+  static const Color darkSurface = Color.fromARGB(255, 27, 27, 27);
+  static const Color error = Color(0xFFff2c2c);
+  static const Color success = Color(0xFF5cb85c);
+  
+  
+  static const Color onPrimary = Colors.white;
+  static const Color onSecondary = Colors.black;
+  static const Color onSurface = Colors.black;
+  static const Color onError = Colors.white;
+  static const Color onSuccess = Colors.black;
+  static const Color onDarkSurface = Colors.white;
+  static const Color onDarkPrimary = Colors.black;
+  static const Color onDarkSecondary = Colors.white;
+  static const Color onDarkError = Colors.white;
+  static const Color onDarkSuccess = Colors.black;
+
+  static const Color background = Color(0xFFf2f2f2);
+  static const Color darkBackground = Color(0xFF1a1a1a);
+}
