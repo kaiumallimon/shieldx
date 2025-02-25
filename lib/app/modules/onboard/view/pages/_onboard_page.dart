15329275f6220d460255bbaa6b44ed8bc51bf4aa
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-26 01:07:25 +0600

    Refactor OnboardPage to use PageView for onboarding screens and improve UI layout

--- lib/app/modules/onboard/view/pages/_onboard_page.dart
+++ lib/app/modules/onboard/view/pages/_onboard_page.dart
@@ -1,47 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shieldx/app/core/constants/_assets.dart';
+import 'package:shieldx/app/core/constants/_sizes.dart';
 import 'package:shieldx/app/core/constants/_strings.dart';
 import 'package:shieldx/app/core/theme/_styles.dart';
-
 import '../../../../core/utils/_system_bar_colors.dart';
+import '../../../../core/widgets/_custom_button.dart';
+import '../../controller/_onboard_controller.dart';
 
 class OnboardPage extends StatelessWidget {
   const OnboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // get theme
     final theme = Theme.of(context).colorScheme;
-
-    // set status bar color and navigation bar color
-    setUiColors(theme);
-
-    // get screen size
     final size = MediaQuery.of(context).size;
+    final controller = Get.find<OnboardController>();
+
+    setUiColors(theme);
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            spacing: 15,
-            children: [
-              // onboard image
-              Image.asset(AppAssets.onboarding1),
-
-              // onboard title
-              Text(
-                AppStrings.onboardingTitle1,
-                style: AppStyles.titleStyle.copyWith(color: theme.onSurface),
-              ),
-
-              // onboard description
-              Text(AppStrings.onboardingSubTitle1),
-
-              // button row
-            ],
-          ),
+        child: Column(
+          children: [
+            Expanded(
+                child: PageView.builder(
+              controller: controller.pageController,
+              onPageChanged: controller.changePage,
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppSize.sidePadding,
+                    vertical: AppSize.topBottomPadding,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        controller.getImage(index),
+                        fit: BoxFit.contain,
+                        height: size.height * 0.4,
+                      ),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: AppSize.insidePadding,
+                        ),
+                        child: Column(
+                          spacing: 15,
+                          children: [
+                            Text(
+                              controller.getTitle(index),
+                              textAlign: TextAlign.center,
+                              style: AppStyles.titleStyle.copyWith(
+                                color: theme.onSurface,
+                              ),
+                            ),
+                            Text(
+                              controller.getSubtitle(index),
+                              textAlign: TextAlign.center,
+                              style: AppStyles.subtitleStyle.copyWith(
+                                color: theme.onSurface.withOpacity(0.5),
+                                fontSize: AppSize.fontSmall,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          3,
+                          (dotIndex) => Obx(() {
+                            return Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 5),
+                              height: 10,
+                              width: controller.currentPage.value == dotIndex
+                                  ? 30
+                                  : 10,
+                              decoration: BoxDecoration(
+                                  color:
+                                      controller.currentPage.value == dotIndex
+                                          ? theme.primary
+                                          : theme.onSurface.withOpacity(.3),
+                                  // shape: BoxShape.circle,
+                                  borderRadius: BorderRadius.circular(500)),
+                            );
+                          }),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Obx(() {
+                            return CustomButton(
+                              text: controller.currentPage.value < 2
+                                  ? AppStrings.next
+                                  : AppStrings.getStarted,
+                              onPressed: controller.nextPage,
+                              color: theme.primary,
+                              textColor: theme.onPrimary,
+                              width: size.width * 0.5,
+                            );
+                          }),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                );
+              },
+            )),
+          ],
         ),
       ),
     );
