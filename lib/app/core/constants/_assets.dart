7f5c12dfe7c273b844347413376b94ad65aedd90
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-26 00:05:04 +0600

    Add profile picture assets to AppAssets class

--- lib/app/core/constants/_assets.dart
+++ lib/app/core/constants/_assets.dart
@@ -1,5 +1,5 @@
-class AppAssets{
-  // Logo 
+class AppAssets {
+  // Logo
   static const String logosmall = 'assets/images/1.png';
   static const String logobig = 'assets/images/2.png';
   static const String logoWithName = 'assets/images/3.png';
@@ -8,4 +8,9 @@ class AppAssets{
   static const String onboarding1 = 'assets/images/4.png';
   static const String onboarding2 = 'assets/images/5.png';
   static const String onboarding3 = 'assets/images/6.png';
+
+  // profile picture
+  static const String maleProfile = 'assets/images/7.png';
+  static const String femaleProfile = 'assets/images/8.png';
+  static const String avatar = 'assets/images/avatar-placeholder.png';
 }
