b0ad02c1d1509dafa85e322737e660736e95275d
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-26 01:06:40 +0600

    Add AppSize class for centralized size constants management

---
+++ lib/app/core/constants/_sizes.dart
@@ -0,0 +1,37 @@
+class AppSize{
+  static const double fontRegular = 16;
+  static const double fontMedium = 18;
+  static const double fontLarge = 20;
+
+  static const double fontTitle = 24;
+  static const double fontTitleLarge = 28;
+  static const double fontTitleXLarge = 32;
+
+  static const double fontSmall = 14;
+  static const double fontXSmall = 12;
+  static const double fontXXSmall = 10;
+
+  static const double sidePadding = 20;
+  static const double insidePadding = 10;
+  static const double topBottomPadding = 15;
+
+  static const double borderRadius = 10;
+  static const double borderRadiusLarge = 15;
+  static const double borderRadiusXLarge = 20;
+
+  static const double borderWidth = 1;
+  static const double borderWidthLarge = 2;
+
+  static const double iconSize = 20;
+  static const double iconSizeLarge = 25;
+
+  static const double buttonHeight = 50;
+  static const double buttonHeightLarge = 60;
+
+  static const double inputHeight = 50;
+  static const double inputHeightLarge = 60;
+
+
+  static const double cardHorizontalPadding = 20;
+  static const double cardVerticalPadding = 15;
+}
