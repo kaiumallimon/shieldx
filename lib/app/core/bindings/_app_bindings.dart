fb36ed37e193580d2eac0065aff78a9ba47514a6
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-27 01:39:08 +0600

    Add RegisterController to AppBindings for user registration management

--- lib/app/core/bindings/_app_bindings.dart
+++ lib/app/core/bindings/_app_bindings.dart
@@ -2,14 +2,16 @@ import 'package:get/get.dart';
 import 'package:shieldx/app/modules/auth/controller/_login_controller.dart';
 import 'package:shieldx/app/modules/onboard/controller/_onboard_controller.dart';
 
+import '../../modules/auth/controller/_register_controller.dart';
 import '../../modules/splash/controller/_splash_controller.dart';
 
 class AppBindings extends Bindings {
   @override
   void dependencies() {
     // Controllers
-    Get.lazyPut(() => SplashController());
-    Get.lazyPut(() => OnboardController());
-    Get.lazyPut(() => LoginController());
+    Get.put(SplashController());
+    Get.put(OnboardController());
+    Get.put(LoginController());
+    Get.put(RegisterController());
   }
 }
