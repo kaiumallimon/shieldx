94bc9d54b991c249f5102f7cc1aee4622ed5be25
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-27 01:38:25 +0600

    Add dotenv package and initialize environment variables in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shieldx/app/local/_onboard_service.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/_app.dart';
 
 void main() async {
@@ -10,6 +10,10 @@ void main() async {
 
   // initialize hive
   await OnboardService().initialize();
+
+
+  // initialize .env
+  await dotenv.load(fileName: ".env");
 
   // run app
   runApp(const MyApp());
