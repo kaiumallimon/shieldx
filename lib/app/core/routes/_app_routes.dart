527c5b07d540e1c41107ee0f875330d8b50a086f
Kaium Al Limon <limon.3cs@gmail.com>
2025-02-27 00:51:55 +0600

    Add route for registration page

--- lib/app/core/routes/_app_routes.dart
+++ lib/app/core/routes/_app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:shieldx/app/modules/auth/view/pages/_login.dart';
+import 'package:shieldx/app/modules/auth/view/pages/_register.dart';
 
 import '../../modules/onboard/view/pages/_onboard_page.dart';
 import '../../modules/splash/view/pages/_splash_page.dart';
@@ -8,4 +9,6 @@ List<GetPage> appRoutes = [
   GetPage(name: '/', page: () => const SplashPage()),
   GetPage(name: '/onboard', page: () => const OnboardPage()),
   GetPage(name: '/login', page: () => const LoginPage()),
+  GetPage(name: '/register', page: () => const RegisterPage()),
+
 ];
