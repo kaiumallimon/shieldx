e059921641df04119c7bb50e3be13683d815759f
Kaium Al Limon <limon.3cs@gmail.com>
2026-02-01 07:11:49 +0600

    feat: Update VaultPage to include image assets and implement CustomScrollView layout

--- lib/app/features/dashboard/features/vault/pages/_vault_page.dart
+++ lib/app/features/dashboard/features/vault/pages/_vault_page.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 class VaultPage extends StatelessWidget {
   const VaultPage({super.key});
 
+  final List<String> images = const [
+    'assets/images/18930.jpg',
+    'assets/images/143415.jpg',
+    'assets/images/41064.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Home Page',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
+      body: SafeArea(child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            
+          )
+        ],
+      )),
     );
   }
 }
